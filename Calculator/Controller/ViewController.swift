a2715c54d76306c33da3cac90fc029393c758ee6
halil851 <halil851@gmail.com>
2022-12-14 10:55:52 +0300

    Result Label add, before linked #315

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -11,78 +11,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
-    @IBOutlet weak var buttonAC: UIButton!
     
-    private var isDecimalUsed: Bool = false
     private var isFinishedTypingNumber: Bool = true
     
     private var displayValue: Double {
         get {
             guard let number = Double(displayLabel.text!) else {
-                fatalError("Can not convert display label text to Double")
+                fatalError("Cannot convert display label text to a Double.")
             }
             return number
         }
-        
         set {
             resultLabel.text = String(newValue)
-            displayLabel.text = String(newValue)
         }
     }
-
+    
+    private var calculator = CalculatorLogic()
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-
+        
+        //What should happen when a non-number button is pressed
+        
         isFinishedTypingNumber = true
         
-        if displayLabel.text == "." {
-            displayLabel.text = "0."
-        }
-        
-       
+        calculator.setNumber(displayValue)
         
         if let calcMethod = sender.currentTitle {
-    
-            if calcMethod == "AC" {
-                isDecimalUsed = false
-                displayLabel.text = "0"
-                resultLabel.text = "Result"
-            } else if calcMethod == "+/-" {
-                displayValue *= -1
-            } else if calcMethod == "%"{
-                
-                displayValue /= 100
+ 
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
             }
-            
-            
-            
         }
-        
-        
-    
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        
+        //What should happen when a number is entered into the keypad
         
         if let numValue = sender.currentTitle {
-            
-            if numValue == "."{
-                if isDecimalUsed { return }
-                isDecimalUsed = true
-            }
             
             if isFinishedTypingNumber {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
                 displayLabel.text = displayLabel.text! + numValue
             }
-           
         }
-
     }
 
 }
-
